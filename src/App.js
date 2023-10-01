38c87a0c902470c5843570d86abe864fbff4f0d2
Miroslav Uzunov <miroslav.uzunov23@gmail.com>
2023-10-01 22:33:10 +0300

    toastify css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
